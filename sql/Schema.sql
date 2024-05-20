4de20e802dc0a8a80f0d01f0311336b62a51f70c
itzsamr <samarathiban@gmail.com>
2024-05-20 10:23:52 +0530

    commit - Schema.sql

---
+++ sql/Schema.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE OMSDB;
+
+
+USE OMSDB;
+
+
+CREATE TABLE Products ( productId INT PRIMARY KEY, productName NVARCHAR(255), description NVARCHAR(MAX), price FLOAT, quantityInStock INT, type NVARCHAR(50) );
+
+
+CREATE TABLE Users ( userId INT PRIMARY KEY, username NVARCHAR(255), password NVARCHAR(255), role NVARCHAR(50) CHECK (role IN ('Admin', 'User')) );
+
+
+CREATE TABLE [Order] ( OrderId INT IDENTITY(1,1) PRIMARY KEY, UserId INT, FOREIGN KEY (UserId) REFERENCES Users(UserId) );
+
+
+CREATE TABLE OrderProduct ( OrderId INT, ProductId INT, Quantity INT, PRIMARY KEY (OrderId, ProductId), FOREIGN KEY (OrderId) REFERENCES [Order](OrderId), FOREIGN KEY (ProductId) REFERENCES Products(ProductId) );
+
