1230644779b4dd6900d7003f6f331fc9489e29c3
itzsamr <samarathiban@gmail.com>
2024-05-20 11:05:57 +0530

    commit - createUser

--- dao/OrderProcessor.py
+++ dao/OrderProcessor.py
@@ -16,7 +16,33 @@ class OrderProcessor(IOrderManagementRepository):
         pass
 
     def createUser(self, user):
-        pass
+        try:
+            connection = DBConnUtil.getDBConn()
+            if connection:
+                cursor = connection.cursor()
+                cursor.execute(
+                    "SELECT COUNT(*) FROM Users WHERE userId = ?", user.getUserId()
+                )
+                user_count = cursor.fetchone()[0]
+                if user_count > 0:
+                    print(f"User with ID {user.getUserId()} already exists.")
+                    return
+                cursor.execute(
+                    "INSERT INTO Users (userId, username, password, role) VALUES (?, ?, ?, ?)",
+                    user.getUserId(),
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.getRole(),
+                )
+                connection.commit()
+                print("User created successfully.")
+            else:
+                print("Failed to connect to database.")
+        except Exception as e:
+            print("Error creating user:", e)
+        finally:
+            if connection:
+                connection.close()
 
     def getAllProducts(self):
         pass
